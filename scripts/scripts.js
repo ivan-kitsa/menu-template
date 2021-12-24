754277889a4155728253b3410b42d4e956337afe
ivan-kitsa <ivan@zenio.co>
2021-12-24 03:17:50 +0300

    Created: customSelect();

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,31 @@
 'use strict'
 
+function customSelect() {
+    const selectNode = document.getElementById('select')
+    const currentSelect = document.getElementById('current-select')
+    const selectList = document.getElementById('select-list').children
+
+    selectNode.onclick = function (){
+        this.classList.add('open')
+    }
+
+    function setValue(e) {
+        currentSelect.innerText = e.target.id
+    }
+
+    for (let i = 0; i < selectList.length; i++) {
+        selectList[i].onclick = setValue
+    }
+
+    document.addEventListener('click', function (e) {
+        if (!e.target.classList.contains('select')) {
+            selectNode.classList.remove('open')
+        }
+    })
+}
+
+function initUi() {
+    customSelect()
+}
+
+initUi()
