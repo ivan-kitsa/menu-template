af1e81f611f76fc2de0177ae84e3475ac22c32f6
ivan-kitsa <ivan@zenio.co>
2022-01-19 09:44:14 +0300

    Started: main page mobile version; Created: mobile header; Changed: paths of files based on version of layouts;

---
+++ scripts/mob/scripts-mob.js
@@ -0,0 +1,157 @@
+'use strict'
+
+function customSelect() {
+    const selectNode = document.getElementById('select')
+    const currentSelect = document.getElementById('current-select')
+    const selectList = document.getElementById('select-list')?.children
+
+
+    if (!selectNode || !currentSelect || !selectList) {
+        return
+    }
+
+    selectNode.onclick = function (){
+        this.classList.add('open')
+    }
+
+    for (let i = 0; i < selectList.length; i++) {
+        selectList[i].onclick = (e) => currentSelect.innerText = e.target.id
+    }
+
+    document.addEventListener('click', function (e) {
+        if (!e.target.classList.contains('select')) {
+            selectNode.classList.remove('open')
+        }
+    })
+}
+
+function menuSelect() {
+    const selectNode = document.getElementById('menu-select')
+    const selectList = selectNode?.querySelectorAll('.menu-type')
+    const currentSelect = selectNode?.querySelector('.current')
+
+    if (!selectNode || !currentSelect || !selectList) {
+        return
+    }
+
+    selectNode.ontouchend = function (){
+        this.classList.toggle('open')
+    }
+
+    selectList.forEach((s) => {
+        s.ontouchstart = (e) => {
+            selectNode.querySelector('.menu-type.current').classList.remove('current')
+            e.target.classList.add('current')
+        }
+    })
+
+    document.ontouchend = function (e) {
+        if (!e.target?.classList?.contains('menu-select')) {
+            selectNode.classList.remove('open')
+        }
+    }
+}
+
+function headerScrollSizer() {
+    const header = document.querySelector('.header-wrapper')
+
+    if (!header) {
+        return
+    }
+
+    if (header.classList.contains('header-category')) {
+        return
+    }
+
+    if (header.nextElementSibling?.getBoundingClientRect().y < 0) {
+        setTimeout(() => {
+            header.classList.add('header-min')
+            header.classList.remove('init')
+            initScroll()
+        }, 500)
+        return
+    }
+
+    header.classList.remove('init')
+    initScroll()
+
+    function initScroll() {
+        window.addEventListener('scroll', function()  {
+            if (this.scrollY > 40 ) {
+                header.classList.add('header-min')
+                return
+            }
+            header.classList.remove('header-min')
+        })
+    }
+}
+
+function categoriesSmoothScroll() {
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault()
+
+            document.querySelector('.current-category')?.classList.remove('current-category')
+            e.target.classList.add('current-category')
+
+            const href = this.getAttribute('href').substring(1)
+            const scrollTarget = document.getElementById(href)
+
+            if (!scrollTarget) return
+
+            const elementPosition = scrollTarget.getBoundingClientRect().top
+            const offsetPosition = elementPosition - 100
+
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            })
+        })
+    })
+}
+
+function initMenuRadio() {
+    const inputs = document.querySelectorAll('input[type="radio"]')
+
+    if (!inputs || !inputs.length) {
+        return
+    }
+
+    let checkedId = ''
+    inputs.forEach(i => {
+        if (!checkedId && i.checked) {
+            checkedId = i.id
+        }
+        i.addEventListener('input', e => {
+            checkedId = e.target.id
+            setRadio(e.target.id)
+        })
+    })
+}
+
+function setRadio(id) {
+    const menuGroups = document.querySelectorAll('.menu-group')
+
+    if (!menuGroups || !menuGroups.length) {
+        return
+    }
+
+    menuGroups.forEach(i => {
+        if (i.getAttribute('data-group') === id) {
+            i.classList.remove('hidden')
+        } else {
+            i.classList.add('hidden')
+        }
+    })
+    resizeSliders()
+}
+
+function initUi() {
+    menuSelect()
+    customSelect()
+    headerScrollSizer()
+    categoriesSmoothScroll()
+    initMenuRadio()
+}
+
+initUi()
---
+++ scripts/mob/slider-mob.js
@@ -0,0 +1,107 @@
+'use strict'
+
+const initSlidersArr = []
+
+class Slider {
+    constructor(id) {
+        this.id = id
+        this.slider = document.getElementById(this.id)
+        this.cards = this.slider.querySelectorAll('.card')
+        this.leftButton = this.slider.querySelector('.left-button')
+        this.rightButton = this.slider.querySelector('.right-button')
+        this.cardH = 0
+        this.cardW = 0
+        this.sliderW = 0
+    }
+
+
+    setSizes() {
+        const cardCopy = this.slider.querySelector('.card').cloneNode(true)
+        cardCopy.style.cssText = 'position: fixed; left: 0; top: 0; visibility: hidden; z-index: -9999;'
+        document.body.appendChild(cardCopy)
+
+        this.cardH = cardCopy.offsetHeight
+        this.cardW = cardCopy.offsetWidth
+        this.sliderW = document.querySelector('.center-column').offsetWidth
+
+        cardCopy.remove()
+
+        this.slider.querySelector('.cards').style.cssText = `height: ${this.cardH}px`
+    }
+    moveToRight() {
+        for (let i = 0, dataId = 0; i < this.cards.length; i++) {
+            dataId = +this.cards[i].getAttribute('data-id')
+
+            this.cards[i].setAttribute('data-id', `${dataId + 1}`)
+            this.rightButton.classList.remove('hidden')
+
+            if (i === dataId) {
+                this.leftButton.classList.add('hidden')
+            }
+        }
+    }
+    moveToLeft() {
+        for (let i = 0, dataId = 0; i < this.cards.length; i++) {
+            dataId = +this.cards[i].getAttribute('data-id')
+
+            this.cards[i].setAttribute('data-id', `${dataId - 1}`)
+            this.leftButton.classList.remove('hidden')
+
+            if (i === this.cards.length - 2 && dataId === this.getMaxViewsSlides()) {
+                this.rightButton.classList.add('hidden')
+            }
+        }
+    }
+    getMaxViewsSlides() {
+        if (!this.sliderW || !this.cardW) {
+            return 3
+        }
+        return Math.floor(this.sliderW / this.cardW)
+    }
+    init() {
+        this.setSizes()
+        this.rightButton.addEventListener('mousedown', () => this.moveToLeft())
+        this.leftButton.addEventListener('mousedown', () => this.moveToRight())
+        if (this.cards.length - 1 < this.getMaxViewsSlides()) {
+            this.rightButton.classList.add('hidden')
+        }
+    }
+}
+
+function getAllSlidersIds() {
+    const idArr = []
+    const sliders = document.querySelectorAll('.cards-slider')
+    sliders.forEach(i => i.id ? idArr.push(i.id): null)
+    return idArr
+}
+
+function resizeFix() {
+    let doIt = 0
+
+    window.addEventListener('resize', () => {
+        clearTimeout(doIt)
+        doIt = setTimeout(() => {
+            resizeSliders()
+        }, 200)
+    })
+}
+
+function resizeSliders() {
+    const sliders = document.querySelectorAll('.cards-slider')
+    sliders.forEach((s, i) => {
+        initSlidersArr[i].setSizes()
+    })
+}
+
+function initSliders() {
+    const idArr = getAllSlidersIds()
+    for (let i = 0; i < idArr.length; i++) {
+        initSlidersArr.push(new Slider(idArr[i]))
+        initSlidersArr[i].init()
+    }
+    resizeFix()
+}
+
+// initSliders()
+
+
